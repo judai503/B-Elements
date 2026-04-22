419db433e5d08f011e03a2d36e575a6211d11994
El tío judai <alexnarcista@gmail.com>
2026-04-22 14:34:57 -0600

    Update and rename cmds to main.js

---
+++ main.js
@@ -0,0 +1,103 @@
+import ws from 'ws';
+import moment from 'moment';
+import chalk from 'chalk';
+import fs from 'fs';
+import path from 'path';
+import gradient from 'gradient-string';
+import seeCommands from './core/system/commandLoader.js';
+import initDB from './core/system/initDB.js';
+import antilink from './commands/antilink.js';
+import level from './commands/level.js';
+import { getGroupAdmins } from './core/message.js';
+
+seeCommands();
+
+export default async (client, m) => {
+  const sender = m.sender;
+  let body = m.message.conversation || m.message.extendedTextMessage?.text || m.message.imageMessage?.caption || m.message.videoMessage?.caption || m.message.buttonsResponseMessage?.selectedButtonId || m.message.listResponseMessage?.singleSelectReply?.selectedRowId || m.message.templateButtonReplyMessage?.selectedId || '';
+  if ((m.id.startsWith("3EB0") || (m.id.startsWith("BAE5") && m.id.length === 16) || (m.id.startsWith("B24E") && m.id.length === 20))) return
+  initDB(m, client)
+  antilink(client, m);
+
+  const from = m.key.remoteJid;
+  const botJid = client.user.id.split(':')[0] + '@s.whatsapp.net' || client.user.lid;
+  const chat = global.db.data.chats[m.chat] || {}
+  const settings = global.db.data.settings[botJid] || {}
+  const user = global.db.data.users[sender] ||= {}
+  const users = chat.users[sender] || {}
+  const pushname = m.pushName || 'Sin nombre';
+  
+  let groupMetadata = null
+  let groupAdmins = []
+  let groupName = ''
+  if (m.isGroup) {
+    groupMetadata = await client.groupMetadata(m.chat).catch(() => null)
+    groupName = groupMetadata?.subject || ''
+    groupAdmins = groupMetadata?.participants.filter(p => (p.admin === 'admin' || p.admin === 'superadmin')) || []
+  }  
+  const isBotAdmins = m.isGroup ? groupAdmins.some(p => p.phoneNumber === botJid || p.jid === botJid || p.id === botJid || p.lid === botJid ) : false
+  const isAdmins = m.isGroup ? groupAdmins.some(p => p.phoneNumber === sender || p.jid === sender || p.id === sender || p.lid === sender ) : false
+  const isOwners = [botJid, ...(settings.owner ? [settings.owner] : []), ...global.owner.map(num => num + '@s.whatsapp.net')].includes(sender);
+
+  for (const name in global.plugins) {
+    const plugin = global.plugins[name];
+    if (plugin && typeof plugin.all === "function") {
+      try {
+        await plugin.all.call(client, m, { client });
+      } catch (err) {
+        console.error(`Error en plugin.all -> ${name}`, err);
+      }
+    }
+  }
+
+  const today = new Date().toLocaleDateString('es-CO', { timeZone: 'America/Bogota', year: 'numeric', month: '2-digit', day: '2-digit' }).split('/').reverse().join('-');
+  if (!users.stats) users.stats = {};
+  if (!users.stats[today]) users.stats[today] = { msgs: 0, cmds: 0 };
+  users.stats[today].msgs++;
+
+  const rawBotname = settings.namebot || 'Yuki';
+  const tipo = settings.type || 'Sub';
+  const cleanBotname = rawBotname.replace(/[^a-zA-Z0-9\s]/g, '')
+  const namebot = cleanBotname || 'Yuki';
+  const shortForms = [namebot.charAt(0), namebot.split(" ")[0], tipo.split(" ")[0], namebot.split(" ")[0].slice(0, 2), namebot.split(" ")[0].slice(0, 3)];
+  const prefixes = shortForms.map(name => `${name}`);
+  prefixes.unshift(namebot);
+
+  let prefix;
+  if (Array.isArray(settings.prefix) || typeof settings.prefix === 'string') {
+    const prefixArray = Array.isArray(settings.prefix) ? settings.prefix : [settings.prefix];
+    prefix = new RegExp('^(' + prefixes.join('|') + ')?(' + prefixArray.map(p => p.replace(/[.*+?^${}()|[\]\\]/g, '\\$&')).join('|') + ')', 'i');
+  } else if (settings.prefix === true) {
+    prefix = new RegExp('^', 'i');
+  } else {
+    prefix = new RegExp('^(' + prefixes.join('|') + ')?', 'i');
+  }
+
+  const strRegex = (str) => str.replace(/[|\\{}()[\]^$+*?.]/g, '\\$&');
+  let pluginPrefix = client.prefix ? client.prefix : prefix;
+  let matchs = pluginPrefix instanceof RegExp ? [[pluginPrefix.exec(m.text), pluginPrefix]] : Array.isArray(pluginPrefix) ? pluginPrefix.map(p => {
+    let regex = p instanceof RegExp ? p : new RegExp(strRegex(p));
+    return [regex.exec(m.text), regex];
+  }) : typeof pluginPrefix === 'string' ? [[new RegExp(strRegex(pluginPrefix)).exec(m.text), new RegExp(strRegex(pluginPrefix))]] : [[null, null]];
+
+  let match = matchs.find(p => p[0]);
+
+  if (!match) return;
+
+  let usedPrefix = (match[0] || [])[0] || '';
+  let args = m.text.slice(usedPrefix.length).trim().split(" ");
+  let command = (args.shift() || '').toLowerCase().normalize('NFD').replace(/[\u0300-\u036f]/g, '');
+  let text = args.join(' ');
+  if (!command) return;
+
+  const cmdData = global.comandos.get(command);
+  if (!cmdData) return m.reply(`ꕤ El comando *${command}* no existe.`);
+
+  try {
+    await cmdData.run(client, m, args, usedPrefix, command, text);
+  } catch (error) {
+    await client.sendMessage(m.chat, { text: `Error: ${error}` }, { quoted: m });
+  }
+
+  level(m);
+};
